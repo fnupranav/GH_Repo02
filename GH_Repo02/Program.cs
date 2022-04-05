c576a994c87d3653e5335126a4cdfd8413171891
pranavlexisnexis <Pranav.FNU@lexisnexisrisk.com>
2022-04-05 02:30:02 -0500

    first commit into feature branch

--- GH_Repo02/Program.cs
+++ GH_Repo02/Program.cs
@@ -7,6 +7,18 @@ namespace GH_Repo02
         static void Main(string[] args)
         {
             Console.WriteLine("Hello GitHub Upload 2");
+
+            Console.WriteLine("Calculator Inputs");
+            Console.WriteLine("Operations- Input any one from [ADD, SUB, MUL, DIV]: ");
+            var optr = Console.ReadLine();
+            Console.WriteLine("Input first integer:");
+            var a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input second integer:");
+            var b = Convert.ToInt32(Console.ReadLine());
+
+            Calculator calc = new Calculator();
+            string result = calc.operations(optr, a, b).ToString();
+            Console.WriteLine(String.Format("Results: {0}", result));
         }
     }
 }
